2641de022572183cdd92011747c6c6c5f3ecc016
Alexey Kozlov <alexey.kozlov@h-its.org>
2023-10-19 18:01:49 +0200

    update search_DNA56_conspart to include parsimony starting trees + update ground truth

--- test/search_DNA56_conspart.sh
+++ test/search_DNA56_conspart.sh
@@ -1,1 +1,1 @@
-$RAXNG --msa $DATADIR/dna56.phy --model GTR+G --tree rand{5} --tree-constraint $DATADIR/dna56.conspart.tree --outgroup Out2 --prefix $PREFIX --seed 1 --threads $THREADS --redo $NGARGS
+$RAXNG --msa $DATADIR/dna56.phy --model GTR+G --tree pars{5},rand{5} --tree-constraint $DATADIR/dna56.conspart.tree --outgroup Out2 --prefix $PREFIX --seed 1 --threads $THREADS --redo $NGARGS
